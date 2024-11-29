fab02aa47a124cc572d4c60d56c8e4b165a5fcc5
PeterBul <pc.bulukin@gmail.com>
2024-11-29 13:18:50 +0100

    fix: Change title of pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "PAO Tutor: Generate Random Numbers for Memory Training",
+        short_name: "PAO Tutor",
+      },
+    }),
+  ],
   base: "/random-generator/",
 });
